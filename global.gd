0a27206ed2887c34735aa7f7601e3338dce15e10
maxmorrow12 <maxamorr@iu.edu>
2023-09-18 21:42:46 -0400

    Completes part 2

--- global.gd
+++ global.gd
@@ -1,11 +1,7 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	if Input.is_action_pressed("Quit"):
+		get_tree().quit()
